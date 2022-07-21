37054b80922bbf65ff7c4062cb48bc87e13a8685
imlasindu <107388404+imlasindu@users.noreply.github.com>
2022-07-21 15:54:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,6 @@ from shortener import short
 app = Flask(__name__, template_folder="public")
 
 
-@app.route("/")
-def main():
-    if request.args.get('query'):
-        query = request.args.get('query')
-    else:
-        return render_template("index.html")
-    results = short(query)
-    if results is not None:
-        return jsonify(results)
-    else:
-        return jsonify(
-            {"error": "Something wrong"}
-        )
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
